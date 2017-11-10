f321be3f6cf60ce1eb9e8a6d5affac5f101d7898
thedomeffm <domenic.gerhold@reservix.de>
2017-11-10 16:46:47 +0100

    rework2

--- src/AppBundle/Command/AppGenerateStandardCommand.php
+++ src/AppBundle/Command/AppGenerateStandardCommand.php
@@ -28,18 +28,20 @@ class AppGenerateStandardCommand extends ContainerAwareCommand
         $ent1 = new Status();
         $ent1->setName('Elvis - Live');
         $ent1->setUrl('https://elvis.adticket.de/login');
-
+        $em->persist($ent1);
+        /*
         $ent2 = new Status();
         $ent2->setName('Elvis - Test');
         $ent2->setUrl('https://test-elvis.adticket.de/login');
-
+        $em->persist($ent2);
+        */
+        /*
         $ent3 = new Status();
         $ent3->setName('Elvis - Stage');
         $ent3->setUrl('https://stage-elvis.adticket.de/login');
-
-        $em->persist($ent1);
-        $em->persist($ent2);
         $em->persist($ent3);
+        */
+
         $em->flush();
     }
 
